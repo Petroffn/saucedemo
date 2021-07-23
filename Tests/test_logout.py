12b623fe25cb4e933ea8c7dec29431ecf0c2eb69
petroffn <petroff.n@gmail.com>
2021-07-23 22:36:18 +0300

    Polishing test login

--- Tests/test_logout.py
+++ Tests/test_logout.py
@@ -10,18 +10,3 @@ class TestLogout:
         logout_page.click_logout_button()
 
 
-'''class LogoutPage:
-    def __init__(self, driver):
-        self.driver = driver
-
-    @allure.step('Open menu')
-    def move_to_menu(self):
-        self.driver.find_element_by_xpath('//button[@id="react-burger-menu-btn"]').click()
-
-    @allure.step('Click on the Logout button')
-    def click_logout_button(self):
-        self.driver.find_element_by_xpath('//a[@id="logout_sidebar_link"]').click()
-
-    @allure.step('Wait for Login button')
-    def wait_for_login_button(self):
-        self.driver.find_element_by_xpath('//input[@id="login-button"]').click()'''
