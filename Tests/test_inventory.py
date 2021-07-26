08e5500cf34e6758af039dd44a94abf6aa1387b6
petroffn <petroff.n@gmail.com>
2021-07-26 17:04:15 +0300

    Small update

--- Tests/test_inventory.py
+++ Tests/test_inventory.py
@@ -5,10 +5,12 @@ from pages.inventory_page import InventoryPage
 class TestGoods:
     @allure.title('Check that user can add to cart')
     def test_user_can_logout(self, login, driver):
-        goods_page = InventoryPage(driver)
-        goods_page.check_backpack_button_status_is_add_to_cart()
-        goods_page.add_backpack_to_cart()
-        goods_page.button_change_status()
-        goods_page.check_cart_changed_to_one()
-        goods_page.delete_backpack_from_cart()
-        goods_page.check_backpack_button_status_is_add_to_cart()
+        inventory_page = InventoryPage(driver)
+        inventory_page.check_backpack_button_status_is_add_to_cart()
+        inventory_page.add_backpack_to_cart()
+        inventory_page.button_change_status()
+        inventory_page.check_cart_changed_to_one()
+        inventory_page.delete_backpack_from_cart()
+        inventory_page.check_backpack_button_status_is_add_to_cart()
+
+
